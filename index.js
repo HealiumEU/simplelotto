47550fe51a07a0db406636b6596a261f390b8194
Kristian Batalo <k.batalo98@htl-ottakring.ac.at>
2018-07-02 20:51:39 +0200

    Complete Function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 (function(){
     let number = window.prompt('Bitte geben Sie Ihre Zahl an');
-    
+
+    let rndNumber = Math.round((Math.random()*5));
+
+    console.log(`Number Match ${(rndNumber == number)}`)
 })()
